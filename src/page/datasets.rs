f8aae17a850ea7802b6458c0b34a45b99cbf9dc7
Steen Larsen <sla@keycore.dk>
2020-11-16 22:41:38 +0100

    added accordion

--- src/page/datasets.rs
+++ src/page/datasets.rs
@@ -1,5 +1,5 @@
-use chrono::prelude::*;
 use seed::{prelude::*, *};
+use std::collections::HashMap;
 
 // ------ ------
 //     Init
@@ -7,17 +7,7 @@ use seed::{prelude::*, *};
 
 pub fn init(url: Url, _: &mut impl Orders<Msg>) -> Model {
     Model {
-        changes_status: ChangesStatus::NoChanges,
-        errors: Vec::new(),
-
-        form: Form {
-            username: String::new(),
-            email: String::new(),
-            password: String::new(),
-            confirm_password: String::new(),
-
-            errors: FormErrors::default(),
-        },
+       accordion: HashMap::new()
     }
 }
 
@@ -26,33 +16,7 @@ pub fn init(url: Url, _: &mut impl Orders<Msg>) -> Model {
 // ------ ------
 
 pub struct Model {
-    changes_status: ChangesStatus,
-    errors: Vec<FetchError>,
-
-    form: Form,
-}
-
-enum ChangesStatus {
-    NoChanges,
-    Saving { requests_in_flight: usize },
-    Saved(DateTime<Local>),
-}
-
-struct Form {
-    username: String,
-    email: String,
-    password: String,
-    confirm_password: String,
-
-    errors: FormErrors,
-}
-
-#[derive(Default)]
-struct FormErrors {
-    username: Option<String>,
-    email: Option<String>,
-    password: Option<String>,
-    confirm_password: Option<String>,
+    accordion: HashMap<String, String>,
 }
 
 // ------ ------
@@ -60,31 +24,25 @@ struct FormErrors {
 // ------ ------
 
 pub enum Msg {
-    ChangesSaved(Option<FetchError>),
-    ClearErrors,
-
-    UsernameChanged(String),
-    EmailChanged(String),
-    PasswordChanged(String),
-    ConfirmPasswordChanged(String),
-
-    Save,
-    DeleteAccount,
+    Show(String),
 }
 
 pub fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::ChangesSaved(None) => {}
-        Msg::ChangesSaved(Some(fetch_error)) => {}
-        Msg::ClearErrors => {}
-
-        Msg::UsernameChanged(username) => {}
-        Msg::EmailChanged(email) => {}
-        Msg::PasswordChanged(password) => {}
-        Msg::ConfirmPasswordChanged(confirm_password) => {}
-
-        Msg::Save => {}
-        Msg::DeleteAccount => {}
+	Msg::Show(id) => {
+	    match model.accordion.get(&"one".to_string()) {
+		Some(x) =>  {
+		    if x.to_string() == "hidden".to_string() {
+			model.accordion.insert(id, "".to_owned());
+		    } else {
+			model.accordion.insert(id, "hidden".to_owned());
+		    }
+		},
+		None => {
+		    let x = model.accordion.insert(id, "".to_owned());
+		}
+	    }
+	}
     }
 }
 
@@ -92,6 +50,21 @@ pub fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 //     View
 // ------ ------
 
+
+fn accordion(id: &str, title: &str, content: Node<Msg>, model: &Model) -> Node<Msg> {
+    let i = id.to_string();
+    div![C!["rounded-sm"],
+         div![C!["border border-b-0 bg-gray-100 px-10 py-6"],
+	      button![C!["underline text-blue-500 hover:text-blue-700 focus:outline-none"], ev(Ev::Click, |_| Msg::Show(i)), title]],
+	 
+         div![C!["border border-b-0 px-10 py-6"], attrs!{At::Class => model.accordion.get(&id.to_string()).unwrap_or(&"hidden".to_string())},
+	      content]]
+}
+
 pub fn view(model: &Model) -> Node<Msg> {
-    div!["Datasets view"]
+    let a1 = "one".to_string();
+    let content = p!["Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."];
+    div![C!["w-full my-4"],
+	 accordion(&a1, "Collapsible Group Item #1", content, model)
+    ]
 }
